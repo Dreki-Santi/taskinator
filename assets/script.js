b668e20d017c29a29a244b431d9c90fa454cce4e
Mob <dreki.santi@protonmail.com>
2022-01-03 17:32:09 -0700

    Form now adds task

--- assets/script.js
+++ assets/script.js
@@ -7,20 +7,23 @@ window.document.querySelector("button");
 
 var btn = window.document.querySelector("button");
 console.dir(btn);
-*/
+
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+console.log(buttonEl);*/
 //---------------------------------------------------
 
 //Start
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    //console.log(event);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
